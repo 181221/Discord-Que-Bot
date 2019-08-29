bbb1a3a99df5f110871ead9ca45b21c1247c0c4f
181221 <181221@stud.hvl.no>
2019-08-29 11:52:40 +0200

    discord que

---
+++ DiscordBot.py
@@ -0,0 +1,42 @@
+import discord
+import subprocess, sys
+import os
+import time
+from PIL import Image, ImageGrab
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+FILE_NAME="screen.jpg"
+TOKEN = os.getenv("TOKEN")
+PATH_TO_SCRIPT = "C:\\init.ps1"
+POWER_COMMAND = ["powershell.exe", "-ExecutionPolicy", "ByPass", PATH_TO_SCRIPT]
+
+client = discord.Client()
+
+
+def start():
+    p = subprocess.Popen(POWER_COMMAND, stdout=sys.stdout)
+    p.communicate()
+
+
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+    if message.author == client.user:
+        return
+
+    if message.content == "que":
+        await channel.send('Relax, I will que up for you :D')
+        start()
+        time.sleep(30)
+        ImageGrab.grab().save(FILE_NAME, "JPEG")
+        await channel.send(file=discord. File(FILE_NAME))
+        os.remove(FILE_NAME)
+
+
+client.run(TOKEN)
---
+++ init.ps1
@@ -0,0 +1,62 @@
+
+
+$PATH='C:\Program Files (x86)\World of Warcraft\_classic_'
+$hashTable = @{}
+
+Function Get-Environment-Variables {
+	Get-Content .env | Foreach-Object{
+	   $var = $_.Split('=')
+	   New-Variable -Name $var[0] -Value $var[1]
+	   $hashTable.Add($var[0],$var[1])
+	}
+}
+
+Get-Environment-Variables
+
+$ACCOUNT = $hashTable['ACCOUNT']
+$PASSWORD= $hashTable['PASSWORD']
+
+[system.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null
+
+function Click-MouseButton
+{
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(792,228)
+    $signature=@' 
+      [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+      public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@ 
+
+    $SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+
+        $SendMouseClick::mouse_event(0x00000002, 0, 0, 0, 0);
+        $SendMouseClick::mouse_event(0x00000004, 0, 0, 0, 0);
+}
+
+Function Login-WoW
+{
+	.\Wow.exe
+	Start-Sleep -s 3
+
+	$wsh = New-Object -ComObject WScript.Shell
+	$wsh.SendKeys($ACCOUNT)
+	Start-Sleep -s 1
+	$wsh.SendKeys("{TAB}")
+	$wsh.SendKeys($PASSWORD)
+	Start-Sleep -s 1
+	$wsh.SendKeys("{ENTER}")
+	Start-Sleep -s 2
+	$wsh.SendKeys("{ENTER}")
+	Start-Sleep -s 5
+	Click-MouseButton
+	$wsh.SendKeys("{ENTER}")
+}
+
+Function Init 
+{
+	Set-Location -Path $PATH
+	Login-WoW
+}
+
+Init
+
+
